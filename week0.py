cfcc945d606a2584d70a713711fd492aed9a6799
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-02 20:40:06 +0300

    used 'or'

--- week0.py
+++ week0.py
@@ -1,8 +1,6 @@
 x = int(input("What is x?"))
 y = int(input("What is y?"))
-if x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
+if x < y or x > y:
+    print("x is not equal to y")
 else:
-    print("x is equal to py")
+    print("x is equal to y")
