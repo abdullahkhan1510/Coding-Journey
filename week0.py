ecf8c1a8fc94260e56984e2af70577aba4fc3b5e
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-02 20:16:17 +0300

    use an if statement

--- week0.py
+++ week0.py
@@ -1,9 +1,4 @@
-# Gathering inputs
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-
-# Multiplying and dividing by 2
-res = (a * b) / 2
-
-# Output
-print("Result:", res, " if the number is an integer, number is even and if number has a decimal other than .0 it is odd")
+x = int(input("What is x?"))
+y = int(input("What is y?"))
+if x < y:
+    print("x is less than y")
