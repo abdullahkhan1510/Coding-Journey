1e6b9f28ab0ac36c5b1d0eb18bcc483883367a5e
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-08 19:01:06 +0300

    use two functions and convert a time to either breakfast, lunch or dinner or nothing.

--- week0.py
+++ week0.py
@@ -1,8 +1,24 @@
-name = input("What is your name? ")
-match name:
-    case "Harry" | "Hermione" | "Ron":
-        print("Gryffindor")
-    case "Draco":
-        print("Slytherin")
-    case _:
-        print("Who?")
+def main():
+    time = input("What is the time? You must answer in the 24h format. ")
+    converted_time = convert(time)
+    if 7.0 <= converted_time <= 8.00:
+        print("breakfast time")
+    elif 12.0 <= converted_time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= converted_time <= 19.0:
+        print("dinner time")
+    else:
+        print("")
+def convert(time):
+    minutes = int(0)
+    hours, minutes = time.split(":")
+    newh = int(hours)
+    newm = float(minutes)
+    minutes_dec = float(newm / 60)
+    time = float(newh + minutes_dec)
+    return time
+
+
+
+if __name__ == "__main__":
+    main()
