5acdad23739b3241d642debebefd68ae04eca5df
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-11 21:43:25 +0300

    use for loop and range function to print programmers choice

--- week2.py
+++ week2.py
@@ -1,9 +1,2 @@
-i = 1
-password = input("\n What is your password?").strip()
-while i != 0:
-    pass2 = input("Please reenter your original password").strip()
-    if pass2 == password:
-        print("You may proceed.")
-        i == 0
-    else:
-        i = i + 1
+for i in range(3):
+    print("Zubanga")
