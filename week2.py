68b7e2016bd4cc3f5b5abd2950f42ae8e671fff4
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-15 17:11:28 +0300

    use a list od dictionaries

--- week2.py
+++ week2.py
@@ -1,6 +1,8 @@
-students = {
-    "Hermione":"Gryffindor",
-     "Ron":"Gryffindor",
-      "Harry":"Gryffindor",
-       "Drako":"Slytherin",
-}
+students = [
+    {"name":"Hermione", "house":"Gryffindor", "patronus":"otter"},
+    {"name":"Harry", "house":"Gryffindor", "patronus":"Stag"},
+    {"name":"Ron", "house":"Gryffindor", "patronus":"Jack Russel Terrier"},
+    {"name":"Drako", "house":"Slytherin", "patronus": None}
+]
+for i in students:
+    print(i["name"])
