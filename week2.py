c4051ee37e241619802ef15624a082e74dc00898
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-16 17:47:46 +0300

    use two functions to print a row of hashtags.

--- week2.py
+++ week2.py
@@ -1,8 +1,5 @@
-students = [
-    {"name":"Hermione", "house":"Gryffindor", "patronus":"otter"},
-    {"name":"Harry", "house":"Gryffindor", "patronus":"Stag"},
-    {"name":"Ron", "house":"Gryffindor", "patronus":"Jack Russel Terrier"},
-    {"name":"Drako", "house":"Slytherin", "patronus": None}
-]
-for i in students:
-    print(i["name"],i["house"], i["patronus"], sep = ", ")
+def main():
+    print_row(3)
+def print_row(n):
+    print("#\n" * 3, end = "")
+main()
