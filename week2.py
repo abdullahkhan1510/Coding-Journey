c0212f1635c419f3948de200c84209e4e7a87382
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-16 17:55:41 +0300

    use 2d arrays but python version to print a grid of squares.

--- week2.py
+++ week2.py
@@ -1,5 +1,16 @@
 def main():
-    print_row(3)
-def print_row(n):
-    print("#\n" * 3, end = "")
+    print_square(3)
+
+
+def print_square(size):
+
+    #For each row in square
+    for i in range(size):
+
+        #For each column in square
+        for j in range(size):
+
+            #Print brick
+            print("#", end = "")
+        print()
 main()
