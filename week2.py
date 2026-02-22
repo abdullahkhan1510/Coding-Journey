9a3eb70425a212e56744d2c0d0a4287739448440
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-22 20:33:56 +0300

    use a dictionary and use stuff from the FDA for fruits and have a user input their fruit and output the calories per 100g of that specific fruit.

--- week2.py
+++ week2.py
@@ -1,16 +1,26 @@
-def main():
-    print_square(3)
-
-
-def print_square(size):
-
-    #For each row in square
-    for i in range(size):
-
-        #For each column in square
-        for j in range(size):
-
-            #Print brick
-            print("#", end = "")
-        print()
-main()
+fruits = {
+    "apple":"130",
+    "avocado":"50",
+    "banana":"110",
+    "cantaloupe":"50",
+    "grapefruit":"60",
+    "grapes":"90",
+    "honeydew melon":"50",
+    "kiwifruit":"90",
+    "lemon":"15",
+    "lime":"20",
+    "nectarine":"60",
+    "orange":"80",
+    "pear":"100",
+    "pineapple":"50",
+    "plums":"70",
+    "strawberries":"50",
+    "sweet cherries":"100",
+    "tangerine":"50",
+    "watermelon":"80",
+}
+fruit = input("Item: ").lower()
+if fruit in fruits:
+    print("Calories: ", fruits[fruit])
+else:
+    print("")
