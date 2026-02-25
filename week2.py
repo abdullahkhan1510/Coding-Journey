ab6f732abcbf9ce03e6632b4c4f3477648c122fb
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-02-25 20:55:56 +0300

    practice iterating through a list and using an if statement to perform a specific action when a certain name is found.

--- week2.py
+++ week2.py
@@ -1,1 +1,10 @@
-print("hello, world)
+students = ["Hermione", "Harry", "Zulay", "Ron"]
+
+print("Checking the attendance list...")
+
+for student in students:
+    if student == "Zulay":
+    
+        print("Found", student, "! They are the leader today.")
+    else:
+        print(student + " is present.")
