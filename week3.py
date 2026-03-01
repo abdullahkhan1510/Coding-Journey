4da0d37d31fb7ad241fd2c199e29c9cb1e2cf8ef
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-03-01 19:26:03 +0300

    use while and break, code keeps prompting user to enter an integer for x, and when a user enters a non integer code says X isn't an integer and tells user to enter x again, when the correct x value is entered user while loop is breaken out of and X is printed.

--- week3.py
+++ week3.py
@@ -1,7 +1,9 @@
-try:
-    x = int(input("What is your number?"))
-    print("X is: ", x)
-except ValueError:
-    print("X is not an integer")
-else:
-    print("X is: ", x)
+while True:
+    try:
+        x = int(input("What is your number?"))
+        print("X is: ", x)
+    except ValueError:
+        print("X is not an integer")
+    else:
+        break
+print("X is ", x)
