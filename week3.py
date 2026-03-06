d8102586377054290632b91ecdf5e245f465d5b0
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-03-06 21:32:01 +0300

    as part of cs50 problem set 3 created a program to take a fraction from a user and convert it to a percentagem had help from CHATGPT

--- week3.py
+++ week3.py
@@ -1,11 +1,21 @@
-def main():
-    x = get_int("What's x?")
-    print("x is", x)
-
-def get_int(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            pass
-main()
+while True:
+    try:
+        fraction = input("Enter your fraction:")
+        num1, num2 = fraction.split("/")
+        num1 = int(num1)
+        num2 = int(num2)
+        if num1 > num2:
+            continue
+        else:
+            break
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+percent = round(num1/num2*100)
+if percent <= 1:
+    print("E")
+elif percent >= 99:
+    print("F")
+else:
+    print(f"{percent}%")
