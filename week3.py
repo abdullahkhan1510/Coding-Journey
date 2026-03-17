26a5386d02b5d39196ade26932cc1e33c89b7f23
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-03-17 21:17:24 +0300

    practice dictionaries from cs50 shorts to make sure i understand it

--- week3.py
+++ week3.py
@@ -1,38 +1,14 @@
-months = [
-    "January", "February", "March",
-    "April", "May", "June",
-    "July", "August", "September",
-    "October", "November", "December"
-]
+distances = {
+    "Voyager 1": 163,
+    "Voyager 2": 136,
+    "Pioneer 10": 80,
+    "New Horizons": 58,
+    "Pioneer 11": 44
+}
 
-while True:
-    try:
-        date = input("Date: ")
+def main():
+    for name in distances:
+        print(f"{name} is {distances[name]} AU from Earth")
 
-        # Slash format: 9/8/1636
-        if "/" in date:
-            month, day, year = date.split("/")
-            month = int(month)
-            day = int(day)
-            year = int(year)
-
-            # Validate and print
-            if 1 <= month <= 12 and 1 <= day <= 31:
-                print(f"{year}-{month:02}-{day:02}")
-                break
-
-        # Comma format: September 8, 1636
-        elif "," in date:
-            month_name, day, year = date.split(" ")
-            day = int(day.replace(",", ""))
-            year = int(year)
-            month = months.index(month_name) + 1  # Convert month name to number
-
-            # Validate and print
-            if 1 <= month <= 12 and 1 <= day <= 31:
-                print(f"{year}-{month:02}-{day:02}")
-                break
-
-    except:
-        # Invalid input → loop continues
-        pass
+if __name__ == "__main__":
+    main()
