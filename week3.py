4943758e688100e00bc87c78baab6f635e48f9f7
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-03-25 20:37:23 +0300

    takes user input for time and outputs time in correct format, for 24h time.

--- week3.py
+++ week3.py
@@ -1,23 +1,12 @@
-months = [
-    "January", "February", "March", "April",
-    "May", "June", "July", "August",
-    "September", "October", "November", "December"
-]
 while True:
     try:
-        date = input("Date:")
-        if "/" in date:
-            month, day, year = date.split("/")
-            month = int(month)
-            day = int(day)
-            year = int(year)
-            print(f"{year}-{month:02d}-{day:02d}")
-        elif "," in date:
-            month, day, year = date.split(" ")
-            month = month.title()
-            day = int(day.replace(",", ""))
-            year = int(year)
-            month = months.index(month) + 1
-            print(f"{year}-{month:02d}-{day:02d}")
+        time = input("Time: ")
+        hours, minutes = time.split(":")
+        hours = int(hours)
+        minutes = int(minutes)
+
+        if 0 <= hours < 24 and 0 <= minutes < 60:
+            print(f"{hours:02d}:{minutes:02d}")
+            break
     except ValueError:
         pass
