fc2b226ea4af3bc2e549e7249a9628ff3224c61b
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-03-31 20:54:33 +0300

    create a function to replace .title(), instead being called format_name

--- week4.py
+++ week4.py
@@ -1,12 +1,6 @@
-# Simple Temp Converter
-unit = input("Is this Celsius or Fahrenheit? (C/F): ")
-temp = float(input("Enter the degrees: "))
+def format_name(name):
+    return name.title()
 
-if unit == "C" or unit == "c":
-    new_temp = (temp * 9/5) + 32
-    print("In Fahrenheit that is:", new_temp)
-elif unit == "F" or unit == "f":
-    new_temp = (temp - 32) * 5/9
-    print("In Celsius that is:", new_temp)
-else:
-    print("I don't know that unit")
+name = input("Name: ")
+formatted = format_name(name)
+print("Hello,", formatted)
