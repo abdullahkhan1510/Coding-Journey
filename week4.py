949aa2a24931988c6df8d1922d63b70e4ba5bd92
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-04-19 09:14:54 +0300

    calculate the legnth of a number inputted by a user

--- week4.py
+++ week4.py
@@ -1,3 +1,7 @@
-n = int(input("Please enter n: "))
-for i in range(1,n+1):
-    print("*"*n)
+count = 0
+num = int(input("Please enter a number: "))
+while num > 0:
+    num = num // 10
+    count = count + 1
+
+print("The length is: ", count)
