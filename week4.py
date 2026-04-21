ae50dff89849b00729c80fb542273302841b2ed0
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-04-21 21:17:12 +0300

    print only odd numbers using a for loop

--- week4.py
+++ week4.py
@@ -1,10 +1,4 @@
 n = int(input("Enter number: "))
 for i in range(1,n+1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+   if i % 2 != 0:
+      print(i)
