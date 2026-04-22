f1b9845971c53c0e8827332807ecd890765e201f
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-04-22 14:06:20 +0300

    reverse an inputted number using the reverse algorithm and a while loop

--- week4.py
+++ week4.py
@@ -1,11 +1,8 @@
-n = int(input("Please enter your number: "))
-num = n
+n = int(input("Please enter a number: "))
 reverse = 0
 while n > 0:
     digit = n % 10
     reverse = reverse*10+digit
     n = n // 10
-if num == reverse:
-    print(num, "is a palindrome")
-else:
-    print(num, "is not a palindrome")
+
+print(reverse)
