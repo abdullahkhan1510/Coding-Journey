db63ed9b2e49d854bd3e32d22701a58acbba566d
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-04-23 19:02:48 +0300

    take input for a number and sum up any numbers from 1 to 'number' that are divisible by 3 or 5

--- week4.py
+++ week4.py
@@ -1,6 +1,6 @@
 n = int(input("Please enter a number: "))
+total = 0
 for i in range(1,n+1):
-    if i % 2 == 0:
-        print(i, ",Even")
-    elif i % 2 != 0:
-        print(i, ",Odd")
+    if i % 3 == 0 or i % 5 == 0:
+        total = total + i
+print("The total is:", total)
