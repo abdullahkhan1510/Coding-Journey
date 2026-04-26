3e589b1b95a9527978e28d244ae997a957213b9b
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-04-26 21:53:17 +0300

    write code to print"Even" if divisble by 2, print "Seven" if divisible by 7 and print"EvenSeven" if divisible by both.

--- week4.py
+++ week4.py
@@ -1,6 +1,10 @@
 n = int(input("Please enter a number: "))
-total = 0
 for i in range(1,n+1):
-    if i % 3 == 0 or i % 5 == 0:
-        total = total + i
-print("The total is:", total)
+    if i % 2 == 0 and i % 7 == 0:
+        print("EvenSeven")
+    elif i % 2 == 0:
+        print("Even")
+    elif i % 7 == 0:
+        print("Seven")
+    else:
+        print(i)
