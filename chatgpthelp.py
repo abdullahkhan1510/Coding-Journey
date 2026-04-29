4d3ec1abee367fbb261d93708240d684ed058cc9
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-04-29 16:49:47 +0300

    take input for a word and count the number of vowels in that specific word.

--- chatgpthelp.py
+++ chatgpthelp.py
@@ -1,14 +1,14 @@
 count = 0
-def count_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-    
-n = int(input("Please enter a number: "))
-for i in range(1,n+1):
-    if count_even(i) == True:
+word = input("Please enter a word: ").lower()
+for i in word:
+    if i in "aeiou":
         count += 1
-    else:
-        count = count
-print(f"{count} numbers from 1 to {n} are even")
+    elif i in "aeiou":
+        count += 1   
+    elif i in "aeiou":
+        count += 1
+    elif i in "aeiou":
+        count += 1
+    elif i in "aeiou":
+        count += 1
+print(f"Your word has {count} vowels in it.")
