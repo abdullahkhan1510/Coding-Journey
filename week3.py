10fd71a536d3da96f197caa962b15e9aa444fe74
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-15 19:34:28 +0300

    created a program and used a for loop and function to take input for a number and print the square of all numbers from to that number.

--- week3.py
+++ week3.py
@@ -1,10 +1,7 @@
-a = int(input("Please enter the first number: "))
-b = int(input("Please enter the second number: "))
-def is_larger(num1,num2):
-    if num1 > num2:
-        return num1
-    elif num2 > num1:
-        return num2
-    else:
-        return "Numbers are equal"
-print(is_larger(a,b))
+num = int(input("What is your number? "))
+def square(n):
+    return n * n
+
+for i in range(1, num+1):
+    print(i, "-->", square(i))
+    
