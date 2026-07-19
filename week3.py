44bebda7ad241fd1890f2499071945e14b668f71
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-19 18:49:10 +0300

    take input for a word and print the number of vowels present in the word.

--- week3.py
+++ week3.py
@@ -1,9 +1,13 @@
-num = int(input("Please enter a number: "))
-def mot(n):
-    if n % 3 == 0:
-        return "True"
+word = input("Please enter a word: ").lower()
+count = 0
+def is_vowel(letter):
+    if letter in "aeiou":
+        return True
     else:
-        return "False"
-for i in range(1, num+1):
-    if mot(i) == "True":
-        print(i)
+        return False
+
+for letter in word:
+    if is_vowel(letter):
+        count = count + 1
+
+print("The word has", count, "vowels in it")
