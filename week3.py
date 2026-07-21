a211a731a2b01f37679e87127e4b0482b24139a0
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-21 16:58:25 +0300

    ask user for name and remove whitespaces and capitalise names.

--- week3.py
+++ week3.py
@@ -1,5 +1,3 @@
-num = int(input("Please enter a number: "))
-def cube(n):
-    return n * n * n
-numcubed = cube(num)
-print("The number you entered when cubed is,", numcubed)
+name = input("Please enter your full name: ")
+name = name.title().strip()
+print("Hello, ", name)
