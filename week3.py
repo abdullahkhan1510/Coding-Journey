6ed956dd40bce91bc1ab7606f0f41986e7838612
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-23 18:38:46 +0300

    take input for age and use an if statement in a function to determine whether user is adult or minor.

--- week3.py
+++ week3.py
@@ -1,8 +1,7 @@
-a = int(input("Please enter a number: "))
-b = int(input("Please enter a number: "))
-
-def add(a,b):
-    ab = a + b
-    ab = str(ab)
-    return "The sum is " + ab
-print(add(a,b))
+age = int(input("Please enter a number: "))
+def is_adult(age):
+    if age >= 18:
+        return "Adult"
+    else:
+        return "Minor"
+print(is_adult(age))
