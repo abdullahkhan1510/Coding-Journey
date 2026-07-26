95e32ed789e3b932afa57c924691ed2b0b13aa9d
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-26 19:10:32 +0300

    take input for a card number and replace all numbers beside the last 4 numbers with '*' and print them and print the last 4 as is.

--- week3.py
+++ week3.py
@@ -1,14 +1,11 @@
-time = input("Please enter a time: ")
-hours, minutes = time.split(":")
-hours = int(hours)
-minutes = int(minutes)
-minutes = minutes / 60
-timeint = hours + minutes
-if timeint >= 7.0 and timeint <= 8.0:
-    print("Breakfast time")
-elif timeint >= 12.0 and timeint<= 13.0:
-    print("Lunch time")
-elif timeint >= 18.0 and timeint <= 19.0:
-    print("Dinner time")
-else:
-    print("Not time for any meal right now. ")
+card = input("Please enter your credit card number: ")
+cardlen = len(card)
+cardlen = int(cardlen)
+ogdig = 4
+stars = cardlen-ogdig
+
+for i in range(cardlen):
+    if cardlen - i > 4:
+        print("*", end = "")
+    else:
+        print(card[i], end = "")
