f2e00c5db6b883fc32ab285a4fb6010319e573af
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-26 18:44:17 +0300

    recreate cs50 mealtime task in different code according to me.

--- week3.py
+++ week3.py
@@ -1,7 +1,14 @@
-day = input("Please enter a day of the week: ").title()
-def weekend(day):
-    if day == "Sunday" or day == "Saturday":
-        return "Weekend"
-    else:
-        return "Weekday"
-print(weekend(day))
+time = input("Please enter a time: ")
+hours, minutes = time.split(":")
+hours = int(hours)
+minutes = int(minutes)
+minutes = minutes / 60
+timeint = hours + minutes
+if timeint >= 7.0 and timeint <= 8.0:
+    print("Breakfast time")
+elif timeint >= 12.0 and timeint<= 13.0:
+    print("Lunch time")
+elif timeint >= 18.0 and timeint <= 19.0:
+    print("Dinner time")
+else:
+    print("Not time for any meal right now. ")
