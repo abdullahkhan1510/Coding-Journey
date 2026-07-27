85000cc2eaf7f236953699089257fb31b77beed0
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-27 19:20:43 +0300

    do the cs50 coke machine task from problem set 2

--- week3.py
+++ week3.py
@@ -1,10 +1,19 @@
-entered = input("camelCase:")
-enteredlen = len(entered)
-print("Snakecase:", end = "")
-for letter in entered:
-    if letter.isupper():
-        print("_", end = "")
-        letter = letter.lower()
-        print(letter, end = "")
+amountdue = 50
+print("Amount due:", amountdue)
+while amountdue > 0:
+    insert = int(input("Insert Coin: "))
+    if insert == 25:
+        amountdue = amountdue - 25
+        print("Amount due:", amountdue)
+    elif insert == 10:
+        amountdue = amountdue - 10
+        print("Amount due:", amountdue)
+    elif insert == 5:
+        amountdue = amountdue - 5
+        print("Amount due:", amountdue)
     else:
-        print(letter, end = "")
+        amountdue = amountdue
+        print("Amount due:", amountdue)
+        
+
+        
