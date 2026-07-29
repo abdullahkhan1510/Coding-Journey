653c3c79d54e091f216a27ca0d966f38007bda2e
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-29 16:43:54 +0300

    use try and valuerror to ensure user enters an integer and keep prompting them to enter an integer until they do.

--- week3.py
+++ week3.py
@@ -1,26 +1,8 @@
-foods = {
-    "Apple": 130,
-    "Avocado": 50,
-    "Banana": 110,
-    "Cantaloupe": 50,
-    "Grapefruit": 60,
-    "Grapes": 90,
-    "Honeydew Melon": 50,
-    "Kiwi Fruit": 90,
-    "Lemon": 15,
-    "Lime": 20,
-    "Nectarine": 60,
-    "Orange": 80,
-    "Peach": 60,
-    "Pear": 100,
-    "Pineapple": 50,
-    "Plums": 70,
-    "Strawberries": 50,
-    "Sweet Cherries": 100,
-    "Tangerine": 50,
-    "Watermelon": 80
-}
-
-fruit = input("Please enter a fruit: ").strip().title()
-if fruit in foods:
-    print(foods[fruit])
+num = False
+while num == False:
+    try:
+        number = int(input("Number: "))
+        print(number*number)
+        num = True
+    except ValueError:
+        print("Please enter a number. ")
