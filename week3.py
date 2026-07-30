5cd5f234b3e5a284c4e638c2fe30934e0f610f78
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-30 15:37:10 +0300

    did the fuel gauge task from cs50 problem set 3 used exception, different errors and try and while true etc.

--- week3.py
+++ week3.py
@@ -1,8 +1,25 @@
-num = False
-while num == False:
+while True:
     try:
-        number = int(input("Number: "))
-        print(number*number)
-        num = True
+        fraction = input("Please enter your fraction: ")
+        numerator, denominator = fraction.split("/")
+        numerator = int(numerator)
+        denominator = int(denominator)
+        if denominator == 0:
+                    raise ValueError
+        tank = numerator/denominator
+        tank = tank * 100 
+        tank = round(tank)
+        if numerator > denominator:
+            raise ValueError
+        else:
+            if tank <= 1:
+                print("E")
+                break
+            elif tank >= 99:
+                print("F")
+                break
+            else:
+                print(tank, "%", sep = "")
+                break
     except ValueError:
-        print("Please enter a number. ")
+        print("Invalid fraction")
