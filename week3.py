c29bb8565efbc08c06f4cd768af94c2bb49dceaa
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-07-31 19:03:22 +0300

    do the cs50 problem set 3's last task which was extremely challenging, task name is outdated.

--- week3.py
+++ week3.py
@@ -1,15 +1,46 @@
-groceries = {}
-
+months = {
+    "January":1,
+    "February":2,
+    "March":3,
+    "April":4,
+    "May":5,
+    "June":6,
+    "July":7,
+    "August":8,
+    "September":9,
+    "October":10,
+    "November":11,
+    "December":12
+}
 while True:
     try:
-        item = input("Item: ").strip().lower()
-        if item in groceries:
-            groceries[item] = groceries[item] + 1
-        elif item not in groceries:
-            groceries[item] = 1
-
-    except EOFError:
-        print("Sorted list: ")
-        break
-for item in sorted(groceries):
-    print(groceries[item], item.upper(), sep = " --> ")
+        date = input("Please enter a date: ")
+        if "/" in date:
+            month, day, year = date.split("/")
+            if int(month) > 12:
+                raise ValueError
+            if int(day) > 31:
+                raise ValueError
+            if int(day) < 10:
+                day = day.zfill(2)
+            if int(month) < 10:
+                month = month.zfill(2)
+            print(year, month, day, sep="-")
+            break
+        elif "," in date:
+            date = date.strip().title()
+            date = date.replace(",", "")
+            month, day, year = date.split(" ")
+            if month not in months:
+                raise ValueError
+            if int(day) > 31:
+                raise ValueError
+            newmonth = str(months[month])
+            if int(day) < 10:
+                day = day.zfill(2)
+            if int(newmonth) < 10:
+                newmonth = newmonth.zfill(2)
+            print(year, newmonth, day, sep="-")
+            break
+    except (ValueError, KeyError):
+        pass
