01141ae7e995bb592d8210392a0a517b5936b320
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-08-07 20:03:46 +0300

    the for loop iterates through every argument provided in the command line, and each argument is printed out with “hello, my name is” before it. Sys.argv[1:] is used to ensure that arguments 1 through the end of the list are printed.

--- week3.py
+++ week3.py
@@ -1,7 +1,6 @@
 import sys
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments:")
-else:
-    print("Hello, my name is", sys.argv[1])
+
+for arg in sys.argv[1:-1]:
+    print("Hello, my name is:", arg)
