db864feb05028d4c58128a628412af2785d80090
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-08-07 19:46:59 +0300

    use sys.exit function to automatically terminate program if user doesn't enter desired amount of arguments.

--- week3.py
+++ week3.py
@@ -1,6 +1,7 @@
 import sys
-try:
-
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments:")
+else:
     print("Hello, my name is", sys.argv[1])
-except IndexError:
-    print("Nothing at said index")
