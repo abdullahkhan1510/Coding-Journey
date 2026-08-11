09ea210a9458a90a1d409d01bfa48e9baf075718
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-08-11 16:36:31 +0300

    do figlet.py from cs50 with extreme struggle, took a long time and patience and testing.

--- say.py
+++ say.py
@@ -1,5 +1,29 @@
 import sys
-from week4 import hello
+from pyfiglet import Figlet
+import random
 
-if len(sys.argv) == 2:
-    hello(sys.argv[1])
+figlet = Figlet()
+fonts = figlet.getFonts()
+
+if len(sys.argv) == 3:
+    if sys.argv[1] != "-f" and sys.argv[1] != "--font":
+        sys.exit("Invalid usage")
+
+if len(sys.argv) != 1 and len(sys.argv) != 3:
+    sys.exit("Invalid usage")
+elif len(sys.argv) == 1:
+    text = input("Input: ")
+    font_choice = random.choice(fonts)
+    figlet.setFont(font=font_choice)
+    print("Output: ")
+    print(figlet.renderText(text))
+elif len(sys.argv) == 3:
+    font = sys.argv[2]
+    if font in fonts:
+            font_choice = sys.argv[2]
+    else:
+        sys.exit("Invalid usage")
+    text = input("Input: ")
+    figlet.setFont(font=font_choice)
+    print("Output: ")
+    print(figlet.renderText(text))
