d91c46e9b248631ca56a94a64f70fe675dae04ed
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-08-11 18:50:52 +0300

    do adieu from ps4 from week4 of cs50

--- week4.py
+++ week4.py
@@ -1,11 +1,18 @@
-def main():
-    hello("world")
-    goodbye("world")
-
-def hello(name):
-    print(f"hello, {name}")
-
-def goodbye(name):
-    print(f"goodbye, {name}")
-if __name__ == "__main__":
-    main()
+names = []
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
+    except EOFError:
+        break
+length = len(names)
+if length == 1:
+    print()
+    print("Adieu, adieu to", name)
+elif length == 2:
+    print()
+    print("Adieu, adieu, to", names[0], "and", names[1])
+else:
+    last = names[-1]
+    print()
+    print("Adieu, adieu, to", ", ".join(names[:-1]) + ", and", last)
