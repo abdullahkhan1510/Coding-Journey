4041c4b111fdd60d1fca132bbdc2adec13296227
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-08-14 19:03:36 +0300

    finish off problem set 4 from cs50 and do the bitcoin price index task.

--- week4.py
+++ week4.py
@@ -1,43 +1,15 @@
-import random
+import sys
+import requests
+try:
+    if len(sys.argv) < 2:
+        sys.exit("Missing command line argument")
+    n = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+result = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=841a8d832d18985c4d09359d3c1f534aadafa162cfddfa1c5aabab2a6fdfc7ae")
+content = result.json()
+price = content["data"]["priceUsd"]
+price = float(price)
+amount = n*price
+print(f"${amount:,.4f}")
 
-def main():
-    entered = get_level()
-    for i in range(0,10):
-        x = generate_integer(entered)
-        y = generate_integer(entered)
-        for j in range(0,3):
-            user_answer = int(input(f"{x} + {y} = "))
-            sum = x+y
-            if user_answer == sum:
-                break
-            elif j  == 2:
-                print(f"{x} + {y} = {sum}")
-                break
-            elif user_answer != sum:
-                print("EEE")
-
-
-
-def get_level():
-    while True:
-        try:
-            level = int(input("Level: "))
-            if level >= 1 and level <= 3:
-                return level
-        except ValueError:
-            pass
-
-
-def generate_integer(level):
-    if level == 1:
-        return random.randint(0,9)
-    elif level == 2:
-        return random.randint(10,99)
-    elif level == 3:
-        return random.randint(100,999)
-    else:
-        raise ValueError
-
-
-if __name__ == "__main__":
-    main()
