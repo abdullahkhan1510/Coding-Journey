82b82e70935b2d84f4ef56686508c98ec21468c0
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-08-16 17:47:32 +0300

    use pythonic key word assert, assert something is something, e.g. assert that when 2 is used as parameter for square() function, 4 will be returned and if 4 is not returned, print a suitable error message.

--- testcalc.py
+++ testcalc.py
@@ -4,10 +4,14 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4!")
-    elif square(3) != 9:
-        print("3 squared was not 9!")
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared was not 4")
+    try:
+        assert square(3) == 4
+    except AssertionError:
+        print("3 squared was not 9")
 
 if __name__ == "__main__":
     main()
